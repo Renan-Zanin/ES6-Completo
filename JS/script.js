d3beb318d07c35f9784b5d7dbf4e36639c0e3b01
Renan Zanin <renan.oliveira.ismart@gmail.com>
2022-03-11 15:05:29 -0300

    modal refatorado com class

--- JS/script.js
+++ JS/script.js
@@ -1,32 +1,14 @@
-// DATASET
-
-// Adicone um atributo data-anime="show-down" e data-anime = "show-right"
-// a todos as section's com descrição dos animais
-
-// Utilizando estes atributos, adicione a classe sho-down ou show-right a
-// sua respectiva section assim que a mesma aparecer na tela (animacao tab)
-
-// No CSS faça com que show-down anime de cimka para baixo e show-right
-// continue com a mesma animação da esquerda para a direita
-
-// Substitua todas as classes js- por data atributes
-
-// (tudo modificado e adicionado no index.html, animacoes.js e syle.css)
-
-// MODULES (criando módulos das animações para facilitar importar e exportar)
-
 import ScrollSuave from './modules/scroll-suave.js';
 import Accordion from './modules/accordion.js';
-import initAnimaScroll from './modules/scroll-animacao.js';
 import TabNav from './modules/tabnav.js';
-import initType from './modules/typeWritter.js';
-import initModal from './modules/modal.js';
+import Modal from './modules/modal.js';
 import initTooltip from './modules/tooltip.js';
-import initDropDownMenu from './modules/dropdown-menu.js';
+import initDropdownMenu from './modules/dropdown-menu.js';
 import initMenuMobile from './modules/menu-mobile.js';
 import initFuncionamento from './modules/funcionamento.js';
-import initiFetchAnimais from './modules/fetchAnimais.js';
-import initiFetchBitcoin from './modules/fetchBitcoin.js';
+import initFetchAnimais from './modules/fetchAnimais.js';
+import initFetchBitcoin from './modules/fetchBitcoin.js';
+import initAnimacaoScroll from './modules/scroll-animacao.js';
 
 const scrollSuave = new ScrollSuave('[data-menu="suave"] a[href^="#"]');
 scrollSuave.init();
@@ -36,12 +18,14 @@ accordion.init();
 
 const tabNav = new TabNav('[data-tab="menu"] li', '[data-tab="content"] section');
 tabNav.init();
-initAnimaScroll();
-initType();
-initModal();
+
+const modal = new Modal('[data-modal="abrir"]', '[data-modal="fechar"]', '[data-modal="container"]');
+modal.init();
+
 initTooltip();
-initDropDownMenu();
+initDropdownMenu();
 initMenuMobile();
 initFuncionamento();
-initiFetchAnimais();
-initiFetchBitcoin();
+initFetchAnimais();
+initFetchBitcoin();
+initAnimacaoScroll();
